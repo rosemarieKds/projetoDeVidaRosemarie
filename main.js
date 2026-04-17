fb717230d2061f04a769ea52a2d9842a13176ea6
rosemarieKds <145386564+rosemarieKds@users.noreply.github.com>
2026-04-17 13:19:30 -0300

    .

--- main.js
+++ main.js
@@ -12,9 +12,15 @@ for (let i=0; i <botoes.length;i++){
 }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024- 10-05T00:00:00");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
+const tempoObjetivo1 = new Date("2024-02-05T00:00:00");
 
-contadores[0].textContent =calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1,tempoObjetivo1, tempoObjetivo3];
+
+contadores[0].textContent =calculaTempo(tempos[0]);
+contadores[1].textContent =calculaTempo(tempoObjetivo2);
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
